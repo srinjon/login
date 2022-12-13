b22f1e6eb7aeee303a9ce87a1c7c48a41eb88663
Srinjon Sadhukhan <99788429+srinjon@users.noreply.github.com>
2022-12-13 14:52:43 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+include('connection.php');
+include('functions.php');
+$user_data=check_login($con);
+?>
+<html>
+    <head>
+        <title>My Website</title>
+    </head>
+    <body>
+        <a href="logout.php">
+            Logout
+        </a>
+        <h1>This is the index page</h1>
+        <br>
+        Hello, <?php echo $user_data['user_name']; ?>.
+    </body>
+</html>
